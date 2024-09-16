42519b7b33d3921762fcef90f8e4c9d010b9ca6c
naishil19 <naishilpatel19@gmail.com>
2024-09-16 22:53:08 +0530

    All Done

--- src/Prac1_8.kt
+++ src/Prac1_8.kt
@@ -1,4 +1,4 @@
-1. Printing Arrays using Different Methods
+//1. Printing Arrays using Different Methods
 import java.util.Arrays
 
 fun main() {
@@ -16,54 +16,54 @@ fun main() {
     println("Array5: ${array5.joinToString()}")
     println("Array6: ${array6.contentDeepToString()}")
 }
-******************************************************************************************************
-2. Manipulating Arrays with Loops
-fun main() {
-    val array = intArrayOf(19, 42, 9, 21, 23, 4)
-
-    print("Array using range : ")
-    for (i in 0..array.lastIndex) {
-        print("${array[i]} ")
-    }
-    println()
-
-    print("Array using downTo : ")
-    for (i in array.lastIndex downTo 0) {
-        print("${array[i]} ")
-    }
-    println()
-
-    print("Array using until : ")
-    for (i in 0 until array.size) {
-        print("${array[i]} ")
-    }
-    println()
-}
-******************************************************************************************************
-3. Sorting an Array with Built-in Functions
-fun main() {
-    val array = intArrayOf(52, 23, 42, 19, 4)
-    println("Array before sorting : ${array.joinToString()}")
-    array.sort()
-    println("Array after sorting : ${array.joinToString()}")
-}
-******************************************************************************************************
-4. Sorting an Array without Built-in Functions
-fun main() {
-    val array = intArrayOf(52, 23, 42, 19, 4)
-    println("Array before sorting : ${array.joinToString()}")
-    bubbleSort(array)
-    println("Array after sorting : ${array.joinToString()}")
-}
-
-fun bubbleSort(array: IntArray) {
-    for (i in array.indices) {
-        for (j in 0 until array.size - i - 1) {
-            if (array[j] > array[j + 1]) {
-                val temp = array[j]
-                array[j] = array[j + 1]
-                array[j + 1] = temp
-            }
-        }
-    }
-}
+//******************************************************************************************************
+//2. Manipulating Arrays with Loops
+//fun main() {
+//    val array = intArrayOf(19, 42, 9, 21, 23, 4)
+//
+//    print("Array using range : ")
+//    for (i in 0..array.lastIndex) {
+//        print("${array[i]} ")
+//    }
+//    println()
+//
+//    print("Array using downTo : ")
+//    for (i in array.lastIndex downTo 0) {
+//        print("${array[i]} ")
+//    }
+//    println()
+//
+//    print("Array using until : ")
+//    for (i in 0 until array.size) {
+//        print("${array[i]} ")
+//    }
+//    println()
+//}
+//******************************************************************************************************
+//3. Sorting an Array with Built-in Functions
+//fun main() {
+//    val array = intArrayOf(52, 23, 42, 19, 4)
+//    println("Array before sorting : ${array.joinToString()}")
+//    array.sort()
+//    println("Array after sorting : ${array.joinToString()}")
+//}
+//******************************************************************************************************
+//4. Sorting an Array without Built-in Functions
+//fun main() {
+//    val array = intArrayOf(52, 23, 42, 19, 4)
+//    println("Array before sorting : ${array.joinToString()}")
+//    bubbleSort(array)
+//    println("Array after sorting : ${array.joinToString()}")
+//}
+//
+//fun bubbleSort(array: IntArray) {
+//    for (i in array.indices) {
+//        for (j in 0 until array.size - i - 1) {
+//            if (array[j] > array[j + 1]) {
+//                val temp = array[j]
+//                array[j] = array[j + 1]
+//                array[j + 1] = temp
+//            }
+//        }
+//    }
+//}
